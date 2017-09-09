7b7ffdcc76dc84182a3b08dc31b8af893113cf4f
Brian Kloppenborg <brian@pratumlabs.com>
2017-09-09 13:38:09 -0600

    Completed graph-based recursive merging method.

--- python/sro-merge-dat.py
+++ python/sro-merge-dat.py
@@ -4,7 +4,9 @@ import argparse
 import numpy as np
 import itertools
 from scipy.optimize import least_squares
+import matplotlib.pyplot as plt
 import networkx as nx
+from operator import attrgetter
 
 # code cleaning could remove/change this dependency
 from apass import apass_save_dir
@@ -54,7 +56,6 @@ row_order = [[ 1,  2,  5,  6,  9, 10],
 col_order = [5, 37, 7, 38, 17, 39, 19, 40, 35, 41, 33]
 fields.append([31000000, row_order, col_order])
 fields.append([31100000, row_order, col_order])
-fields.append([40000000, row_order, col_order])
 
 def read_sro_dat_file(filename):
     """Reads in a SRO .dat file to a numpy associative array."""
@@ -73,9 +74,102 @@ def main():
 
     start = time.time()
 
+    # read in the data and build a graph:
+    data = read_data(args.input)
+    G = build_graph(data, fields)
+
+    # now process each field and merge things together
+    for field_base_id, row_order, col_order in fields:
+        print "Merging field %s" % (field_base_id)
+
+        field_name = get_field_name(field_base_id)
+        neighbors = nx.all_neighbors(G, field_name)
+        sub_G = G.subgraph(neighbors)
+
+        # find the node with the most edges
+        edges = sub_G.edges(data=True)
+        edges = sorted(edges, key=lambda x: x[2]['weight'], reverse=True)
+
+        start_node_id = edges[0][0]
+        merge_neighbors(start_node_id, data, sub_G)
+
+        # verify that everyone was merged in
+        for node_id in sub_G.nodes():
+            if G.node[node_id]['merged'] != True:
+                print "Warning: Node %i was not merged!" % (node_id)
+
+    write_sro_dat('pAllZones.dat', data)
+
+def merge_neighbors(node_id_i, data, G):
+
+    # get a list of all edges attached to this node and sort them in descending
+    # order by weight
+    edges = G.edges(node_id_i, data=True)
+    edges = sorted(edges, key=lambda x: x[2]['weight'], reverse=True)
+
+    for edge in edges:
+        node_id_i = edge[0]
+        node_id_j = edge[1]
+
+        # skip nodes that have already been merged
+        if G.node[node_id_i]['merged'] == True \
+           and G.node[node_id_j]['merged'] == True:
+            continue
+
+        # merge the pointings and mark the edge as merged
+        merge_pointings(data, node_id_i, node_id_j, edge[2]['row_ids'])
+        G.node[node_id_i]['merged'] = True
+        G.node[node_id_j]['merged'] = True
+
+        # recursively merge in other nodes
+        merge_neighbors(node_id_j, data, G)
+
+
+
+def merge_pointings(data, field_i, field_j, data_row_pairs):
+    """ Computes a least squares fit between the data found in the rows identified
+    in data_row_pairs and then merges field_j into the photometric system of
+    field_i
+    """
+
+    print " Merging %i %i" % (field_i, field_j)
+
+    # copy the data we will merge out of the main data array
+    indexes = np.where(data['field_id'] == field_j)
+    merge_field_data = data[indexes]
+
+    # get the indices for the overlapping row entries
+    i,j = map(list, zip(*data_row_pairs))
+    for filter_id in sro_filter_names:
+
+        x, y, x_sig, y_sig = get_good_data(data, i, j, filter_id)
+
+        # run a least-squares fit between the two fields
+        params = [0]
+        results = least_squares(delta_mag_func, params, args=(x, y, x_sig, y_sig))
+        params = results.x
+
+        print "  %3s delta: %+f" % (filter_id, params[0])
+
+        # find filter measurements in the merge field that are valid, copy them
+        # out of the array, apply the delta, and copy them back.
+        good_value_indexes = np.where(merge_field_data[filter_id] <= BAD_MAG_VALUE)
+        good_rows = merge_field_data[good_value_indexes]
+        good_rows[filter_id] += params[0]
+        merge_field_data[good_value_indexes] = good_rows
+
+    # copy the data we modified back into the main data array
+    data[indexes] = merge_field_data
+
+
+def get_field_name(field_id):
+    return "field_" + str(field_id)
+
+def read_data(filenames):
+
     # read in the data
     data = []
-    for filename in args.input:
+    for filename in filenames:
         print "Reading %s" % (filename)
         t_data = read_sro_dat_file(filename)
 
@@ -87,165 +181,91 @@ def main():
     # convert back to a numpy array to make the rest of the logic easier to implement
     data = np.asarray(data, dtype={'names': sro_col_names, 'formats': sro_col_types})
 
-    # Now go through each field and optimize.
+    return data
+
+def build_graph(data, fields):
+    """Builds a NetworkX graph data structure from the data.
+    """
+
+    # Find all overlapping entries. We can compare the RA/DEC values
+    # directly because the sro-rect-to-data.py script ensures all
+    # entries in the same container were written using the same RA/DEC
+    # value
+    ra  = data[0]['ra']
+    dec = data[0]['dec']
+    overlaps = [] # will store all overlaps after the loop completes
+    t_overlaps = [(0, data[0]['field_id'])] # stores successive overlaps temporally
+    for i in range(1, len(data)):
+        if data[i]['ra'] == ra and data[i]['dec'] == dec:
+            t_overlaps.append((i, data[i]['field_id']))
+        else:
+            for j in range(0, len(t_overlaps)):
+                j_row_id, j_field_id = t_overlaps[j]
+                for k in range(j + 1, len(t_overlaps)):
+                    k_row_id, k_field_id = t_overlaps[k]
+
+                    overlaps.append([j_field_id, j_row_id, k_field_id, k_row_id])
+
+            # clear it
+            t_overlaps = [(i, data[i]['field_id'])]
+
+        # update/increment the RA?DEC
+        ra  = data[i]['ra']
+        dec = data[i]['dec']
+
+    # Store the data into a ndarray to accelerate lookups later
+    names = ['field_id0', 'row_id0', 'field_id1', 'row_id1']
+    types = ['int', 'int', 'int', 'int']
+    overlaps = np.core.records.fromrecords(overlaps, names=names, formats=types)
+
+    # Now build the graph. Note, in the end it is possible that some nodes might
+    # not be connected to any other nodes. We'll deal with that later.
+    G = nx.Graph()
+
+    # Add all of the nodes. Also create a field_name node so we can quickly
+    # find unconnected nodes later
     for field_base_id, row_order, col_order in fields:
-        print "Processing pointings for field %i" % (field_base_id)
 
-        # Add the field base ID to the row order and col order lists to
-        # properly reference the field + pointing ID in the data
-        row_order = [[(x + field_base_id) for x in row] for row in row_order]
-        col_order = [x + field_base_id for x in col_order]
+        field_name = "field_" + str(field_base_id)
+        G.add_node(field_name)
 
-        # generate a (unique) list of all of the field + pointing IDs required
-        # for merging
-        field_ids = list(itertools.chain(*row_order))
-        field_ids.extend(col_order)
-        field_ids = list(set(field_ids))
+        for row in row_order:
+            for element in row:
+                node_id = field_base_id + element
+                G.add_node(node_id, merged=False)
+                G.add_edge(field_name, node_id)
 
-        # Select data that is only from this field (and all of its pointings)
-        indexes = np.in1d(data['field_id'], field_ids)
-        #print indexes
-        t_data = data[indexes]
+        for element in col_order:
+            node_id = field_base_id + element
+            G.add_node(node_id, merged=False)
+            G.add_edge(field_name, node_id)
 
-        # Find all overlapping entries. We can compare the RA/DEC values
-        # directly because the sro-rect-to-data.py script ensures all
-        # entries in the same container were written using the same RA/DEC
-        # value
-        ra  = t_data[0]['ra']
-        dec = t_data[0]['dec']
-        overlaps = [] # will store all overlaps after the loop completes
-        t_overlaps = [(0, t_data[0]['field_id'])] # stores successive overlaps temporally
-        for i in range(1, len(t_data)):
-            if t_data[i]['ra'] == ra and t_data[i]['dec'] == dec:
-                t_overlaps.append((i, t_data[i]['field_id']))
-            else:
-                for j in range(0, len(t_overlaps)):
-                    j_row_id, j_field_id = t_overlaps[j]
-                    for k in range(j + 1, len(t_overlaps)):
-                        k_row_id, k_field_id = t_overlaps[k]
+    # add edges to the graph
+    for row in overlaps:
 
-                        overlaps.append([j_field_id, j_row_id, k_field_id, k_row_id, False])
+        src_field  = row['field_id0']
+        src_row    = row['row_id0']
+        dest_field = row['field_id1']
+        dest_row   = row['row_id1']
 
-                # clear it
-                t_overlaps = [(i, t_data[i]['field_id'])]
+        # attempt to find the edge, if it doesn't exist, create it.
+        edge = None
+        try:
+            edge = G[src_field][dest_field]
+        except:
+            G.add_edge(src_field, dest_field, row_ids=[], weight=0, merged=False)
+            edge = G[src_field][dest_field]
 
-            # update/increment the RA?DEC
-            ra  = t_data[i]['ra']
-            dec = t_data[i]['dec']
+        edge['row_ids'].append((src_row, dest_row))
+        edge['weight'] = len(edge['row_ids'])
 
-        # Store the data into a ndarray to accelerate lookups later
-        names = ['field_id0', 'row_id0', 'field_id1', 'row_id1', 'merged']
-        types = ['int', 'int', 'int', 'int', 'bool']
-        overlaps = np.core.records.fromrecords(overlaps, names=names, formats=types)
-        print "Found %i stars that overlap between fields" % (len(overlaps))
+    # the graph is built, we're golden.
+    return G
 
-        # enable to dump unique overlap pairs to the console
-        #temp = zip(overlaps['field_id0'].tolist(), overlaps['field_id1'].tolist())
-        #temp = set(temp)
-        #for entry in temp:
-        #    print entry
-
-        # Merge the data together.
-        # NOTE: Neither one of these functions works correctly on the 308 field.
-        # This is going to be replaced, but preserved in Git.
-#        merge_in_order(t_data, row_order, col_order, overlaps)
-#        merge_neighboring(t_data, row_order, col_order, overlaps)
-
-        # save the results back to the main array
-        data[indexes] = t_data
-
-        # save the results to a file
-        write_sro_dat(apass_save_dir + "p%i" % (field_base_id) + ".dat", t_data)
 
 def write_sro_dat(filename, data):
     print "Saving %s" % (filename)
     np.savetxt(filename, data, fmt=sro_col_formats)
-
-def merge_neighboring(data, row_order, col_order, overlaps):
-
-    start_cell = col_order[0]
-    merge_neighbors(data, start_cell, overlaps)
-
-def merge_neighbors(data, cell_id, overlaps):
-
-    # TODO: rewrite this function to operate directly on overlaps?
-    indexes = np.where(overlaps['field_id0'] == cell_id)
-    merge_cells = overlaps[indexes]
-
-
-    for entry in merge_cells:
-        if entry['merged'] == True:
-            continue
-
-        other_id = entry['field_id1']
-        merge_pointings(data, cell_id, other_id, overlaps)
-        merge_neighbors(data, other_id, overlaps)
-
-    overlaps[indexes] = merge_cells
-
-    print overlaps
-    quit()
-
-def merge_in_order(data, row_order, col_order, overlaps):
-    """Merges the field pointings together into a coherent frame in three stages:
-       1) Merge together the vertical connecting strip
-       2) Merge in each entry to the left of the connecting strip one at a time
-       3) Merge in each entry to the right of the connecting strip one at a time.
-    """
-    row_width = len(row_order[0])
-    num_col_entries = len(col_order)
-
-    # merge the connecting column together first.
-    for i in range(0, num_col_entries - 1):
-        merge_pointings(data, col_order[i], col_order[i+1], overlaps)
-
-    # find the index of the first column value in  the first row
-    connecting_index = 0
-    connecting_cell_value = col_order[0]
-    for i in range(0, row_width):
-        if row_order[0][i] == connecting_cell_value:
-            connecting_index = i
-
-    # merge values to the left and right in each row
-    for row in row_order:
-
-        # go to the left of the connecting column
-        for i in range(connecting_index, 0, -1):
-            merge_pointings(data, row[i], row[i - 1], overlaps)
-
-        # go to the right of the connecting column
-        for i in range(connecting_index, row_width - 1):
-            merge_pointings(data, row[i], row[i + 1], overlaps)
-
-    return
-
-def merge_pointings(data, i, j, overlaps):
-
-    print " Merging %i %i" % (i, j)
-
-    indexes = np.where((overlaps['field_id0'] == i) & (overlaps['field_id1'] == j))
-    t_overlaps = overlaps[indexes]
-
-    for filter_id in sro_filter_names:
-        data_indices_i = t_overlaps['row_id0']
-        data_indices_j = t_overlaps['row_id1']
-
-        x, y, x_sig, y_sig = get_good_data(data, data_indices_i, data_indices_j, filter_id)
-
-        # run a least-squares fit between the two fields
-        params = [0]
-        results = least_squares(delta_mag_func, params, args=(x, y, x_sig, y_sig))
-        params = results.x
-
-        print "  %3s delta: %+f" % (filter_id, params[0])
-
-        # apply the fit to field j
-        indexes = np.where(data['field_id'] == j)
-        t_data  = data[indexes]
-        t_data[filter_id] += params[0]
-
-        # flag it as merged
-        t_overlaps['merged'] = True
 
 def get_good_data(data, i, j, filter_id):
 
