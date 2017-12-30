e304044bd45077236ce3d0b7cff4dbbf3e66155a
Brian Kloppenborg <brian@pratumlabs.com>
2017-12-30 11:13:53 -0700

    Refactor: Extract fred/fredbin I/O to separate file.

--- python/apass.py
+++ python/apass.py
@@ -6,7 +6,6 @@ from numpy import cos
 from math import pi
 import re
 import json
-from copy import copy
 
 # global configuration settings for the APASS Project
 ccd_radius = 2500
@@ -24,63 +23,6 @@ polar_zone_cutoff = 88 # |dec| greater than this are considered part of the pola
 # (static) IDs for the polar zones. Don't change these
 south_zone_id = 0
 north_zone_id = 1
-
-
-# FRED files have the following format:
-# STANDARD MAGNITUDES ONLY
-# FILCON ver 3.3
-# RA (J2000)    DEC        CCDX      CCDY  Flags   HJD      Airmass   Set      Group   Object                   Filt   Mag    Error    dmag    sys night
-# 105.4134694   0.6743509  2996.030    31.010 0 0 56029.599560 1.310    1          2 10040L                        8  16.5515  0.2880  0.0391   232 56029
-fred_col_names = ['ra', 'dec', 'ccdx', 'ccdy', 'flag1', 'flag2', 'hjd', 'airmass', 'set', 'group', 'field', 'filter_id', 'xmag1', 'xerr1', 'dmag', 'sys', 'night']
-fred_col_types = ['float64', 'float64', 'float32', 'float32', 'bool', 'bool', 'float32', 'float32', 'int32', 'int32', 'S25', 'uint8', 'float32', 'float32', 'float32', 'int32', 'int32']
-
-# fredbin follows the same format as fred, but also has columns for 'rect' and 'container'
-fredbin_col_names = copy(fred_col_names)
-fredbin_extra_cols = ['zone_id', 'node_id', 'container_id']
-fredbin_col_names.extend(fredbin_extra_cols)
-fredbin_col_types = copy(fred_col_types)
-fredbin_extra_types = ['int32', 'int32', 'int32']
-fredbin_col_types.extend(fredbin_extra_types)
-fredbin_savetxt_fmt = ['%+011.6f', '%+011.6f', '%011.6f', '%011.6f', '%d', '%d', '%02.6f', '%02.6f', '%6i', '%6i', '%11s', '%03i', '%02.6f', '%02.6f', '%02.6f', '%6i', '%6i', '%6i', '%6i', '%6i']
-
-# data format for output data
-data_col_names = ['name', 'ra', 'ra_err', 'dec', 'dec_err', 'nobs', 'mobs', 'mag1', 'mag2', 'mag3', 'mag4', 'mag5', 'mag6', 'mag_err1', 'mag_err2', 'mag_err3', 'mag_err4', 'mag_err5', 'mag_err6']
-data_col_types = ['int', 'float64', 'float64', 'float64', 'float64', 'int', 'int', \
-                  'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32']
-
-def compare_fred_data(A, B):
-    """Compares two FRED entries stored as numpy structured arrays.
-    Returns True if they match identically, False otherwise."""
-
-    same_point = True
-
-    for key in fred_col_names:
-        if A[key] != B[key]:
-            same_point = False
-
-    return same_point
-
-def read_data(filename):
-    """Reads in an output data file (e.g. a .dat file)"""
-    dtype={'names': data_col_names, 'formats': data_col_types}
-    data = np.loadtxt(filename, dtype=dtype)
-    return data
-
-def read_fred(filename):
-    """Reads in an APASS FRED file."""
-    dtype={'names': fred_col_names,'formats': fred_col_types}
-    data = np.loadtxt(filename, dtype=dtype)
-
-    # append extra type columns for zone_id, node_id, and container_id
-    tmp = np.zeros(len(data))
-    data = nprf.append_fields(data, fredbin_extra_cols, [tmp, tmp, tmp], dtypes=fredbin_extra_types)
-
-    return data
-
-def read_fredbin(filename):
-    """Reads in an APASS .fredbin file"""
-    dtype={'names': fredbin_col_names,'formats': fredbin_col_types}
-    return np.fromfile(filename, dtype)
 
 def get_coords_raw(ra, dec):
     """Returns (ra', dec') = (ra*cos(dec), dec)"""
